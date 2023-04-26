6a8b109a8b6c16d58cfb392c34f2d91ca365a6d7
yelo-o <flyordig@gmail.com>
2023-04-26 23:16:15 +0900

    문자열 및 특수문자 학습

---
+++ src/chap_03/_Quiz_03.java
@@ -0,0 +1,10 @@
+package chap_03;
+
+public class _Quiz_03 {
+    public static void main(String[] args) {
+        String id = "901231-1234567";
+
+        System.out.println(id.substring(0,8));
+        System.out.println(id.substring(0, id.indexOf("-") + 2)); // 0 위치부터 하이픈 위치 +2 직전까지
+    }
+}
