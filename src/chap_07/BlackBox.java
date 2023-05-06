f5d25dc52289d47c2d6c6acfe486a9be5fc51b1f
yelo-o <flyordig@gmail.com>
2023-05-06 22:51:11 +0900

    메소드 autoReport : 전달값과 반환값이 없는 메소드 insertMemoryCard : 전달값이 있는 메소드 getVideFileCount : 전달값과 반환값이 있는 메소드

--- src/chap_07/BlackBox.java
+++ src/chap_07/BlackBox.java
@@ -4,5 +4,31 @@ public class BlackBox {
     String modelName; // 모델명
     String resolution; // 해상도
     int price; // 가격
-    String color; //
+    String color; // 색상
+    static boolean canAutoReport = false; // 자동 신고 기능 (클래스 변수)
+
+    void autoReport() {
+        if (canAutoReport) {
+            System.out.println("충돌이 감지되어 자동으로 신고합니다.");
+        }
+        else {
+            System.out.println("자동 신고 기능이 지원되지 않습니다.");
+        }
+    }
+
+    void insertMemoryCard(int capacity) {
+        System.out.println("메모리 카드가 삽입되었습니다.");
+        System.out.println("용량은" + capacity + "GB 입니다.");
+    }
+
+    int getVideFileCount(int type) {
+        if (type == 1) { // 일반 영상
+            return 9;
+        }
+        else if (type == 2) { // 이벤트 영상
+            return 1;
+        }
+        return 10;
+    }
+
 }
